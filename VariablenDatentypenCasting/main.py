ec16ea1233081c5c29cb2a8a68564744ad4d82a1
ElJanogu <85028459+ElJanogu@users.noreply.github.com>
2021-07-15 15:50:55 +0200

    Add files via upload

---
+++ VariablenDatentypenCasting/main.py
@@ -0,0 +1,66 @@
+print("Programm startet")
+# das ist ein kommentar
+
+x = 5
+print(x)
+print(type(x))
+
+x = 5.6
+print(x)
+print(type(x))
+
+x = "5"
+print(x)
+print(type(x))
+
+y = str(43)
+print(y)
+print(type(y))
+
+y = float("43.5")
+print(y)
+print(type(y))
+
+y = int(3.65)
+print(y)
+print(type(y))
+
+e = "Hallo Welt"
+E = 'Hallo Welt'
+
+print(e)
+print(type(e))
+
+print(E)
+print(type(E))
+
+# string = zeichenketten
+# int = ganze zahlen
+# float = komma zahlen
+
+a = 2
+b = 4
+
+c = a + b
+print(c)
+
+c += a + b # c = c + a + b
+print(c)
+
+a = 4.2
+b = 1.9
+
+c = a + b
+print(c)
+
+c += a + b # c = c + a + b
+print(c)
+
+a = "Hallo"
+b = 'Welt'
+
+c = a + " " + b
+print(c)
+
+c += " !!!"
+print(c)
